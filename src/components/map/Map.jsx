59821a4e726b4ee062915745d7cb312b27e68f31
BuHogeJI2 <dmitrydemidovich@yandex.ru>
2021-01-09 04:50:46 +0300

    Add InfoWindow when clocking on marker Changed:  - fix bug with showing along marker;

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-import {GoogleMap, withGoogleMap, withScriptjs, Marker} from "react-google-maps";
+import {GoogleMap, withGoogleMap, withScriptjs, Marker, InfoWindow} from "react-google-maps";
 import MarkerClusterer from "react-google-maps/lib/components/addons/MarkerClusterer";
 
 const MINSK_COORDS = {lat: 53.904541, lng: 27.561523}
@@ -7,6 +7,7 @@ const MINSK_COORDS = {lat: 53.904541, lng: 27.561523}
 const Map = withScriptjs(withGoogleMap((props) => {
 
     let [stopsTxt, setStopsTxt] = useState('');
+    let [currentStop, setCurrentStop] = useState(null);
 
     const getStopsData = (data) => {
         const result = []
@@ -21,14 +22,21 @@ const Map = withScriptjs(withGoogleMap((props) => {
 
     const returnMarker = (data) => {
         if (data.length === 10) {
-            const stopName = data[0];
+            const stopName = data[4];
             const stopCoords = {
                 lat: +data[7] / 100000,
                 lng: +data[6] / 100000
             }
 
-            return <Marker position={stopCoords} />
+            return <Marker position={stopCoords}
+                           onClick={ () => setCurrentStopData(stopName, stopCoords) }
+            />
         }
+    }
+
+    const setCurrentStopData = (stopName, stopCoords) => {
+        const stopData = {stopCoords, stopName};
+        setCurrentStop(stopData);
     }
 
     useEffect(() => {
@@ -39,12 +47,21 @@ const Map = withScriptjs(withGoogleMap((props) => {
 
     return (
         <GoogleMap defaultZoom={5} defaultCenter={MINSK_COORDS}>
-            <MarkerClusterer onClick={() => console.log('click')}
-                             averageCenter
+            <MarkerClusterer averageCenter
+                             maxZoom={18}
+                             minimumClusterSize={4}
                              enableRetinaIcons
                              gridSize={60} >
                 {getStopsData(stopsTxt).map(data => returnMarker(data))}
             </MarkerClusterer>
+            {currentStop && currentStop.stopName &&
+                <InfoWindow position={ currentStop.stopCoords } onCloseClick={ ()=> setCurrentStop(null) }>
+                    <div>
+                        {currentStop.stopName}
+                        {console.log('here')}
+                    </div>
+                </InfoWindow>
+            }
         </GoogleMap>
     )
 }))
